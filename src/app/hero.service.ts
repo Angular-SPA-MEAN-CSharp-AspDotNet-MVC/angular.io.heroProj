60158e95468215b7f1cf411ef1d585250c2041d1
K. H <knhg2013@gmail.com>
2019-02-11 16:11:44 -0500

    1)Completed the routing works

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,14 @@
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes'
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { MessageService } from './message.service'
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
@@ -14,5 +16,11 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
       this.messageService.add('HeroService: fetch heroes');
       return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // ToDo: send msg after fetching the hero
+    this.messageService.add(`HeroService: fetched here id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service'
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -23,7 +23,4 @@ export class HeroesComponent implements OnInit {
         .subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
